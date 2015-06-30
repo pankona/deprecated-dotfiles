ebc1d159f2e4eb98918c11b2c3aa57eda17a076e
pankona <yosuke.akatsuka@gmail.com>
2015-06-30 20:00:20 +0900

    add startup.sh

---
+++ startup.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+synclient PalmDetect=1 PalmMinWidth=0 PalmMinZ=150
+#synclient PalmDetect=1 PalmMinWidth=1 PalmMinZ=1
+synclient TapButton2=3
+synclient TapButton3=2
+synclient HorizTwoFingerScroll=1
+# x range: 1472-5664
+# y range: 1408-4700
+#LeftEdge                = 1765
+#RightEdge               = 5371
+#TopEdge                 = 1638
+#BottomEdge              = 4470
+
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Area" 1765 5371 0 0
+
+xset r rate 300 25
+
+sh /home/aka/bin/power_save.sh
